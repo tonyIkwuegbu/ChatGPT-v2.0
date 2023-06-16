7eb3f1103ce00264681ed2a298bcae1064da1fb9
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2023-06-16 09:13:43 +0100

    update model

--- src/components/ModelSelection.tsx
+++ src/components/ModelSelection.tsx
@@ -7,7 +7,7 @@ const fetchModels = () => fetch("/api/getEngines").then((res) => res.json());
 const ModelSelection = () => {
 	const { data: models, isLoading } = useSWR("models", fetchModels);
 	const { data: model, mutate: setModel } = useSWR("model", {
-		fallbackData: "text-davinci-003",
+		fallbackData: "text-davinci-002-render-sha",
 	});
 	return (
 		<div>
